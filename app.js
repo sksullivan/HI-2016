830111aee17be981d273fdd32908872d45444057
Stephen Sullivan <sullivankstephen@gmail.com>
2016-02-20 02:38:19 -0600

    lol monet

--- app.js
+++ app.js
@@ -1,60 +1,44 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+/* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    port = 8080,
+    url  = 'http://localhost:' + port + '/';
+/* We can access nodejitsu enviroment variables from process.env */
+/* Note: the SUBDOMAIN variable will always be defined for a nodejitsu app */
+if(process.env.SUBDOMAIN){
+  url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+server.listen(port);
+console.log("Express server listening on port " + port);
+console.log(url);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
+//Socket.io emits this event when a connection is made.
+io.sockets.on('connection', function (socket) {
+  console.log("Client connected");
+  // Emit a message to send it to the client.
+  socket.emit('ping', { msg: 'Hello. I know socket.io.' });
 
-module.exports = app;
+  // Print messages from the client.
+  socket.on('pong', function (data) {
+    console.log(data.msg);
+  });
+
+  function timeout() {
+    setInterval(function() {
+     socket.emit('ping', { msg: 'Hello. I know socket.io.' });
+     console.log("Pinging client");
+     //timeout()
+    }, 2000);
+  }
+  timeout();
+
+});
